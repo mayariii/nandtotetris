059f7aeca02cd805b573e8464fae985023e4463a
Nicole Cox <nicoleanalisecox@gmail.com>
2021-01-02 22:37:12 +0000

    Multiplication program implementation

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,39 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@R1
+D=M     //copy value from R1 into D
+
+@i
+M=D     // set the value of i to value of D from R1
+
+@result
+M=0     // set result = 0
+
+(LOOP)
+@i
+D=M     // copy the current value of i into D
+
+@END
+D;JEQ   // breakpoint - if i = 0 go to END otherwise execute next step
+
+@R0
+D=M     //copy the value from R0 into D
+
+@result
+M=D+M   // compute (value in D from R0 + current value in result) and set as new value of M
+
+@i
+M=M-1   // (i--) compute the current value in i - 1 and set that as new value of M
+
+@LOOP
+0;JMP   // unconditional loop - jump to LOOP
+
+(END)
+@result
+D=M     // copy the value of result into D
+@R2
+M=D     // set the value of R2 to value of D from result
+
+@END
+0;JMP   // infinite loop to end to stop malicious code
